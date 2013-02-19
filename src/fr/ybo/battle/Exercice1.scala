c30c9db4ba7709c9c8944ee8b9db2493dcd7dc7d
ybonnel <ybonnel@gmail.com>
2013-02-19 08:50:53 +0100

    Optim Hanoi

--- src/fr/ybo/battle/Exercice1.scala
+++ src/fr/ybo/battle/Exercice1.scala
@@ -1,153 +1,89 @@
 package fr.ybo.battle
 
-class Hanoi(val pile1: Seq[Int],
-            val pile2: Seq[Int],
-            val pile3: Seq[Int],
+import java.util.concurrent.TimeUnit
+
+class Hanoi(val tours: Seq[Seq[Int]],
             val history: Seq[Hanoi]) {
 
   def isFinished: Boolean = {
-    pile1.isEmpty && pile2.isEmpty
+    tours.zipWithIndex.filter(_._2 != tours.size - 1).filter(!_._1.isEmpty).isEmpty
   }
 
-  def displayState {
-    println("Tour 1 : " + pile1.sorted)
-    println("Tour 2 : " + pile2.sorted)
-    println("Tour 3 : " + pile3.sorted)
+  def displayState() {
+    println(tours.map(tour => {
+      "{" + tour.sorted.mkString(",") + "}"
+    }).mkString(" "))
   }
 
-  def isSame(newHanoi: Hanoi): Boolean = {
-    return (newHanoi.pile1.sorted == this.pile1.sorted
-      && newHanoi.pile2.sorted == this.pile2.sorted
-      && newHanoi.pile3.sorted == this.pile3.sorted)
-  }
-
-  def displaySolution {
+  def displaySolution() {
     println("Nombre de coups : " + history.size)
-    history.foreach(_.displayState)
-    this.displayState
+    history.foreach(_.displayState())
+    this.displayState()
   }
 
   def play: Seq[Hanoi] = {
-    val minPile1 = pile1.reduceOption(_.min(_)).getOrElse(99)
-    val minPile2 = pile2.reduceOption(_.min(_)).getOrElse(99)
-    val minPile3 = pile3.reduceOption(_.min(_)).getOrElse(99)
 
-    var result: Seq[Hanoi] = Seq()
+    var toursCanPlay = tours.zipWithIndex
+    var otherTours = Seq.empty[(Seq[Int], Int)]
 
-    if (minPile1 < minPile2) {
-      // Pile1 => Pile2
-      val newHanoi = new Hanoi(pile1.filter(_ != minPile1),
-        pile2.union(Seq(minPile1)),
-        pile3,
-        history.union(Seq(this)))
+    var result = Seq.empty[Hanoi]
 
-      if (!history.find(hanoi => hanoi.isSame(newHanoi)).isDefined) {
-        result = result.union(Seq(newHanoi))
-      }
+    while (toursCanPlay.size > 1 && toursCanPlay.find(!_._1.isEmpty).isDefined) {
 
-    } else if (minPile2 < minPile1) {
-      // Pile2 => Pile1
-      val newHanoi = new Hanoi(
-        pile1.union(Seq(minPile2)),
-        pile2.filter(_ != minPile2),
-        pile3,
-        history.union(Seq(this)))
+      val minTour = toursCanPlay.minBy(_._1.reduceOption(_.min(_)).getOrElse(99))
 
-      if (!history.find(hanoi => hanoi.isSame(newHanoi)).isDefined) {
-        result = result.union(Seq(newHanoi))
-      }
-    }
-    if (minPile1 < minPile3) {
-      // Pile1 => Pile3
-      val newHanoi = new Hanoi(
-        pile1.filter(_ != minPile1),
-        pile2,
-        pile3.union(Seq(minPile1)),
-        history.union(Seq(this)))
+      toursCanPlay = toursCanPlay.filter(_._2 != minTour._2)
+      val min = minTour._1.min
+      val newHistory = history.union(Seq(this))
+      val minTourWithoutMin = (minTour._1.filter(_ != min), minTour._2)
+      val otherToursWithMin = otherTours.union(Seq(minTourWithoutMin))
 
-      if (!history.find(hanoi => hanoi.isSame(newHanoi)).isDefined) {
-        result = result.union(Seq(newHanoi))
-      }
-    } else if (minPile3 < minPile1) {
-      // Pile3 => Pile1
-      val newHanoi = new Hanoi(
-        pile1.union(Seq(minPile3)),
-        pile2,
-        pile3.filter(_ != minPile3),
-        history.union(Seq(this)))
+      result = result.union(toursCanPlay.map(tourCanPlay => {
+        val toursCanPlayWithoutCurrentTour = toursCanPlay.filter(_._2 != tourCanPlay._2)
+        val tourCanPlayWithMin = (tourCanPlay._1.union(Seq(min)), tourCanPlay._2)
+        new Hanoi(otherToursWithMin.union(toursCanPlayWithoutCurrentTour).union(Seq(tourCanPlayWithMin)).sortBy(_._2).map(_._1)
+          , newHistory)
+      }))
 
-      if (!history.find(hanoi => hanoi.isSame(newHanoi)).isDefined) {
-        result = result.union(Seq(newHanoi))
-      }
+      otherTours :+= minTour
     }
 
-    if (minPile2 < minPile3) {
-      // Pile2 => Pile3
-      val newHanoi = new Hanoi(
-        pile1,
-        pile2.filter(_ != minPile2),
-        pile3.union(Seq(minPile2)),
-        history.union(Seq(this)))
-
-      if (!history.find(hanoi => hanoi.isSame(newHanoi)).isDefined) {
-        result = result.union(Seq(newHanoi))
-      }
-    } else if (minPile3 < minPile2) {
-      // Pile3 => Pile2
-      val newHanoi = new Hanoi(
-        pile1,
-        pile2.union(Seq(minPile3)),
-        pile3.filter(_ != minPile3),
-        history.union(Seq(this)))
-
-      if (!history.find(hanoi => hanoi.isSame(newHanoi)).isDefined) {
-        result = result.union(Seq(newHanoi))
-      }
-    }
-
-    return result
+    result
   }
 }
 
 object Exercice1 {
 
-  def findSolutionForANumber(number:Int) {
-    var allHanoi = Seq(new Hanoi(Seq.range(1, number + 1), Seq(), Seq(), Seq()))
+  def findSolutionForANumber(number: Int): Hanoi = {
+    var allHanoi = Seq(new Hanoi(Seq(Seq.range(1, number + 1), Seq(), Seq()), Seq()))
 
     while (!allHanoi.find(hanoi => hanoi.isFinished).isDefined) {
-      allHanoi = allHanoi.flatMap(hanoi => hanoi.play)
-
-      var newHanois = Seq.empty[Hanoi]
-      allHanoi.filter(hanoi => {
-        val oldHanoi = newHanois.find(_.isSame(hanoi))
-        if (oldHanoi.isDefined) {
-          if (oldHanoi.get.history.size > hanoi.history.size) {
-            newHanois = newHanois.filter(_.isSame(hanoi)).union(Seq(hanoi))
-          }
-          false
-        } else {
-          newHanois :+= hanoi
-          true
-        }
-      })
-      allHanoi = newHanois
+      allHanoi = allHanoi.flatMap(hanoi => hanoi.play).groupBy(hanoi => {
+        hanoi.tours
+      }).map(allSameHanoi => {
+        allSameHanoi._2.minBy(_.history.size)
+      }).toSeq
     }
 
-    val solution = allHanoi.find(hanoi => hanoi.isFinished).get
+    allHanoi.find(hanoi => hanoi.isFinished).get
+  }
 
-    solution.displaySolution
-
+  def timer[A](f: => A) = {
+    val startTime = System.nanoTime
+    val result = f
+    println("Time : " + TimeUnit.NANOSECONDS.toMillis(System.nanoTime - startTime) + "ms")
+    result
   }
 
   def main(args: Array[String]) {
-
-    println("Recherche de solution pour 6")
-    findSolutionForANumber(6)
-
     println("Recherche de solution pour 7")
-    findSolutionForANumber(7)
+    val result7 = timer(findSolutionForANumber(7))
+    result7.displaySolution()
+    assert(result7.history.size == 127)
 
     println("Recherche de solution pour 8")
-    findSolutionForANumber(8)
+    val result8 = timer(findSolutionForANumber(8))
+    result8.displaySolution()
+    assert(result8.history.size == 255)
   }
 }
